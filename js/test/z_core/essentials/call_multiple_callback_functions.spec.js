5fd03d3d7ca76c93e73ca313bec2379930584045
Sergey <nonexistent@example.com>
2014-03-25 14:24:51 +0600

    call_multiple_callback_functions() tests

--- js/test/z_core/essentials/call_multiple_callback_functions.spec.js
+++ js/test/z_core/essentials/call_multiple_callback_functions.spec.js
@@ -48,30 +48,30 @@ function (jsgui, assert, test_utils) {
         //	2 async function call
         // -----------------------------------------------------
 
-        xit("should call the function asynchronously, pass the parameters, and get the results in right order", function (done) {
+        it("should call the function asynchronously, pass the parameters, and get the results in right order", function (done) {
 
             var log = [];
 
             var callback = function (err, res) {
                 assert.equal(err, null);
-                assert.deepEqual(log, ["task2", "task1"]);
-                assert.deepEqual(res, [444, 333]);
+                assert.deepEqual(log, ["task2", "task1"]); // task2 called before the task1 because of the setTimeout() timeout
+                assert.deepEqual(res, [111, 222]); // task1 result, then task2 result
                 done();
             };
 
             var task1_fn = function (arg1, arg2, cb) {
-                setTimeout(function () { log.push("task1"); cb(null, (arg1 * 2) + arg2); }, 10);
+                setTimeout(function () { log.push("task1"); cb(null, (arg2 - arg1)); }, 10); // result: 111
             };
             var task1_args = [111, 222];
 
             var task2_fn = function (arg1, cb) {
-                setTimeout(function () { log.push("task2"); cb(null, arg1); }, 1);
+                setTimeout(function () { log.push("task2"); cb(null, arg1 / 2); }, 1); // result: 222
             };
-            var task2_args = [333];
+            var task2_args = [444];
 
             var tasks = [[task1_fn, task1_args], [task2_fn, task2_args]];
 
-            jsgui.call_multiple_callback_functions(tasks, 2, callback);
+            jsgui.call_multiple_callback_functions(tasks, 300, callback);
 
         });
 
@@ -81,9 +81,8 @@ function (jsgui, assert, test_utils) {
 
         it("should call the functions one by one if the num_parallel is not specified (or if num_parallel == 1)", function (done) {
 
-            var log = [];
-
             var fns = [];
+            var log = [];
 
             for (var c = 0; c < 32; c++) {
                 (function () {
@@ -103,7 +102,9 @@ function (jsgui, assert, test_utils) {
                     throw err;
                 } else {
                     //console.log('test_async_timeouts all done');
-                    //assert.deepEqual(log, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, ]);
+                    //
+                    // make sure all the functions was executed (in the predefined order):
+                    //assert.deepEqual(log, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,... ]);
                     assert.equal(log.length, fns.length);
                     for (var i = 0; i < log.length; i++) {
                         assert.equal(log[i], i);
@@ -119,41 +120,316 @@ function (jsgui, assert, test_utils) {
         //	test_async_timeouts_multi_8
         // -----------------------------------------------------
 
-        it("should ...", function (done) {
-
-            var log = [];
+        it("should execute all the functions (allowing up to 8 a the same time), then call the callback when all the functions are finished", function (done) {
 
             var fns = [];
+            var log = [];
 
             for (var c = 0; c < 32; c++) {
                 (function () {
                     var i = c;
-                    fns.push([function (cb) {
+                    fns.push([function (callback) {
                         setTimeout(function () {
                             //console.log(i + ' complete');
                             log.push(i);
-                            cb();
+                            callback(null, true);
                         }, 1)
                     }, []]);
                 }());
             }
 
-            jsgui.call_multiple_callback_functions(fns, function (err, res) {
+            // arr, fn
+            // arr, num, fn - number is the number of parallel to execute at once.
+            jsgui.call_multiple_callback_functions(fns, 8, function (err, res) {
                 if (err) {
                     throw err;
                 } else {
-                    //console.log('test_async_timeouts all done');
-                    //assert.deepEqual(log, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, ]);
+                    //console.log('test_async_timeouts_multi_8 all done');
+                    //console.log(log);
+                    //
+                    // make sure all the functions was executed (in any order):
                     assert.equal(log.length, fns.length);
                     for (var i = 0; i < log.length; i++) {
-                        assert.equal(log[i], i);
+                        assert.ok(log.indexOf(i) >= 0);
                     }
+                    //
                     done();
                 }
             });
 
 
         });
+
+
+        // ================================================================================================================================
+        //	                        test different call signatures (i.e. polymorphic versions)
+        // ================================================================================================================================
+
+        // -----------------------------------------------------
+        //	(a,f)
+        // -----------------------------------------------------
+
+        it("should process (tasks, callback) parameters", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                done();
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_fn, task1_args]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+        // -----------------------------------------------------
+        //	(a,n,f)
+        // -----------------------------------------------------
+
+        it("should process (tasks, num_parallel, callback) parameters", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                done();
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_fn, task1_args]];
+
+            jsgui.call_multiple_callback_functions(tasks, 100, callback);
+
+        });
+
+
+        // -----------------------------------------------------
+        //	(a,f,b)
+        // -----------------------------------------------------
+
+        it("should process (tasks, callback, return_params) parameters", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [[[111], 222]]);
+                done();
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_fn, task1_args]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback, true);
+
+        });
+
+        // ================================================================================================================================
+        //	                        test different task signatures
+        // ================================================================================================================================
+
+        // -----------------------------------------------------
+        //	[context, fn]
+        // -----------------------------------------------------
+
+        it("should process [context, fn] task signature", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                done();
+            };
+
+            var task1_obj = { calc: function (x) { return 222; } };
+
+            var task1_fn = function (cb) {
+                var obj = this;
+                setTimeout(function () { cb(null, obj.calc()); }, 1);
+            };
+
+            var tasks = [[task1_obj, task1_fn]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+        // -----------------------------------------------------
+        //	[fn, params]
+        // -----------------------------------------------------
+
+        it("should process [fn, params] task signature", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                done();
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_fn, task1_args]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+        // -----------------------------------------------------
+        //	[fn, params, fn_callback]
+        // -----------------------------------------------------
+
+        it("should process [fn, params, fn_callback] task signature", function (done) {
+
+            var task1_callback_called = false;
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                assert.ok(task1_callback_called);
+                done();
+            };
+
+            var task1_callback = function (err, res) {
+                assert.equal(err, null);
+                assert.equal(res, 222);
+                task1_callback_called = true;
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_fn, task1_args, task1_callback]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+        it("should process [fn, params, fn_callback] task signature (2 tasks test)", function (done) {
+
+            var log = [];
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(log, ["task2", "task1"]);
+                assert.deepEqual(res, [222, 444]);
+                done();
+            };
+
+            // task1
+
+            var task1_callback = function (err, res) {
+                assert.equal(err, null);
+                assert.equal(res, 222);
+                log.push("task1");
+            };
+
+            var task1_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 * 2)); }, 10);
+            };
+            var task1_args = [111];
+
+            // task2
+
+            var task2_callback = function (err, res) {
+                assert.equal(err, null);
+                assert.equal(res, 444);
+                log.push("task2");
+            };
+
+            var task2_fn = function (arg1, cb) {
+                setTimeout(function () { cb(null, (arg1 / 2)); }, 1);
+            };
+            var task2_args = [888];
+
+            //
+
+            var tasks = [
+                [task1_fn, task1_args, task1_callback],
+                [task2_fn, task2_args, task2_callback],
+            ];
+
+            jsgui.call_multiple_callback_functions(tasks, 2, callback);
+
+        });
+
+        // -----------------------------------------------------
+        //	[context, fn, params]
+        // -----------------------------------------------------
+
+        it("should process [context, fn, params] task signature", function (done) {
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                done();
+            };
+
+            var task1_obj = { calc: function (x) { return x * 2; } };
+
+            var task1_fn = function (arg1, cb) {
+                var obj = this;
+                setTimeout(function () { cb(null, obj.calc(arg1)); }, 1);
+            };
+            var task1_args = [111];
+
+            var tasks = [[task1_obj, task1_fn, task1_args]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+        // -----------------------------------------------------
+        //	[context, fn, params, fn_callback]
+        // -----------------------------------------------------
+
+        it("should process [context, fn, params, fn_callback] task signature", function (done) {
+
+            var task1_callback_called = false;
+
+            var callback = function (err, res) {
+                assert.equal(err, null);
+                assert.deepEqual(res, [222]);
+                assert.ok(task1_callback_called);
+                done();
+            };
+
+            var task1_obj = { calc: function (x) { return x * 2; } };
+
+            var task1_callback = function (err, res) {
+                assert.equal(err, null);
+                assert.equal(res, 222);
+                task1_callback_called = true;
+            };
+
+            var task1_fn = function (arg1, cb) {
+                var obj = this;
+                setTimeout(function () { cb(null, obj.calc(arg1)); }, 1);
+            };
+
+            var task1_args = [111];
+
+            var tasks = [[task1_obj, task1_fn, task1_args, task1_callback]];
+
+            jsgui.call_multiple_callback_functions(tasks, callback);
+
+        });
+
+
 
     });
 
