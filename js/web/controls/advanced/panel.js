a8d2a8aae92620b6e4cc3ce2972ea871377ee5cc
James Vickers <james@metabench.com>
2015-03-29 16:41:57 +0100

    Panel improvements

--- js/web/controls/advanced/panel.js
+++ js/web/controls/advanced/panel.js
@@ -14,57 +14,68 @@ define(["../../jsgui-html", "./horizontal-menu"],
 */
 
 var jsgui = require('../../jsgui-html');
-var Horizontal_Menu = require('./horizontal-menu');
+//var Horizontal_Menu = require('./horizontal-menu');
 
-        var stringify = jsgui.stringify, each = jsgui.each, tof = jsgui.tof, is_defined = jsgui.is_defined;
-        var Control = jsgui.Control;
+var stringify = jsgui.stringify, each = jsgui.each, tof = jsgui.tof, is_defined = jsgui.is_defined;
+var Control = jsgui.Control;
 
-        var group = jsgui.group;
+var group = jsgui.group;
 
-        var Panel = Control.extend({
-            // panel name?
+// Titled_Panel would be useful.
+//  Would extend the panel, and also show it's name or title.
 
-            // could have a title field.
-            'fields': {
-                'name': String
-            },
-
-            // maybe add before make would be better. add will probably be used more.
-            'init': function(spec, add, make) {
-                this._super(spec);
-
-                this.__type_name = 'panel';
-
-                this.set('dom.attributes.class', 'panel');
-                //console.log('spec.el', spec.el);
-
-                if (!spec.abstract && !spec.el) {
+// Want to keep panel simple. Could have Titled_Panel, maybe Resizable_Panel.
+//  If we want a panel with a lot of functionality, it would be the Flexi_Panel.
 
 
-                    /*
-                    var ctrl_fields = {
-                        'ctrl_relative': div_relative._id(),
-                        'title_bar': title_bar._id()
-                    }
 
 
-                    this.set('dom.attributes.data-jsgui-ctrl-fields', stringify(ctrl_fields).replace(/"/g, "'"));
-                    */
 
 
-                }
+var Panel = Control.extend({
+    // panel name?
 
-            },
-            //'resizable': function() {
-            //},
-            'activate': function() {
-                // May need to register Flexiboard in some way on the client.
-                this._super();
+    // could have a title field.
+    'fields': {
+        'name': String
+    },
 
-                //
+    // maybe add before make would be better. add will probably be used more.
+    'init': function(spec, add, make) {
+        this._super(spec);
 
+        this.__type_name = 'panel';
+
+        this.set('dom.attributes.class', 'panel');
+        //console.log('spec.el', spec.el);
+
+        if (!spec.abstract && !spec.el) {
+
+
+            /*
+            var ctrl_fields = {
+                'ctrl_relative': div_relative._id(),
+                'title_bar': title_bar._id()
             }
-        })
+
+
+            this.set('dom.attributes.data-jsgui-ctrl-fields', stringify(ctrl_fields).replace(/"/g, "'"));
+            */
+
+
+        }
+
+    },
+    //'resizable': function() {
+    //},
+    'activate': function() {
+        // May need to register Flexiboard in some way on the client.
+        this._super();
+
+        //
+
+    }
+})
 
 module.exports = Panel;
 /*
--- js/web/controls/advanced/title-bar.js
+++ js/web/controls/advanced/title-bar.js
@@ -1,42 +1,26 @@
+var jsgui = require('../../jsgui-html');
+var stringify = jsgui.stringify, each = jsgui.each, tof = jsgui.tof;
+var Control = jsgui.Control;
 
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
-
-define(["../../jsgui-html", "./single-line"], 
-	function(jsgui, Single_Line) {
-		
-		var stringify = jsgui.stringify, each = jsgui.each, tof = jsgui.tof;
-		var Control = jsgui.Control;
-		
-
-		// Extending, with field values being set?
-		//  Setting field values in definitions may be a useful thing.
-		var Title_Bar = Control.extend({
-			// fields... text, value, type?
-			//  type could specify some kind of validation, or also 'password'.
-				
-			// single field?
-			'fields': [
-				['text', String]
-			],			
-			//  and can have other fields possibly.
-			
-			
-			'init': function(spec) {
-				this._super(spec);
-
-				this.set('dom.attributes.class', 'title bar');
-				var span = new jsgui.span({
-					'context': this._context
-				})
-				span.add(this.get('text').value());
-				//ctrl_title_bar.set('dom.attributes.class', 'titlebar');
-				this.add(span);
-				
-			}
-		});
-		return Title_Bar;
-		
-		//return jsgui;
-		
-	
+// Extending, with field values being set?
+//  Setting field values in definitions may be a useful thing.
+var Title_Bar = Control.extend({
+    // fields... text, value, type?
+    //  type could specify some kind of validation, or also 'password'.
+    // single field?
+    'fields': [
+        ['text', String]
+    ],
+    //  and can have other fields possibly.
+    'init': function(spec) {
+        this._super(spec);
+        this.set('dom.attributes.class', 'title bar');
+        var span = new jsgui.span({
+            'context': this._context
+        })
+        span.add(this.get('text').value());
+        //ctrl_title_bar.set('dom.attributes.class', 'titlebar');
+        this.add(span);
+    }
 });
+module.exports = Title_Bar;
