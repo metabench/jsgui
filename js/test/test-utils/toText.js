ffe74003201e452cbd112ee791466309eb2dc07a
Sergey <nonexistent@example.com>
2015-11-18 19:06:44 +0500

    toText.toText() improve

--- js/test/test-utils/toText.js
+++ js/test/test-utils/toText.js
@@ -1,18 +1,26 @@
 ﻿
-if (typeof define !== 'function') {
-    var define = require('amdefine')(module);
-}
+//if (typeof define !== 'function') {
+//    var define = require('amdefine')(module);
+//}
 
-define([
-    '../../core/jsgui-data-structures',
-    '../../core/collection',
-    '../../core/jsgui-data-structures-doubly-linked-list'
-], function (
-    jsguiDataStructures,
-    Collection,
-    Doubly_Linked_List
-    ) {
+//define([
+//    '../../core/jsgui-data-structures',
+//    '../../core/collection',
+//    '../../core/jsgui-data-structures-doubly-linked-list'
+//], function (
+//    jsguiDataStructures,
+//    Collection,
+//    Doubly_Linked_List
+//    ) {
 
+
+    var jsguiDataStructures = require('../../core/jsgui-data-structures');
+    var Doubly_Linked_List = require('../../core/jsgui-data-structures-doubly-linked-list');
+
+    var Data_Value = require('../../core/data-value');
+    var Data_Object = require('../../core/data-object');
+    var Enhanced_Data_Object = require('../../core/enhanced-data-object');
+    var Collection = require('../../core/collection');
 
     function toText(v) {
         var text = "" + v; //if (v && v.stringify) text = v.stringify();
@@ -31,17 +39,27 @@ define([
         }
         //
         if (v instanceof jsguiDataStructures.Ordered_KVS) text = "Ordered_KVS:" + text;
-        if (v instanceof Collection) text = "Collection:" + text;
         if (v instanceof Doubly_Linked_List) text = "Doubly_Linked_List:" + text;
         if (v instanceof Doubly_Linked_List.Node) text = "Doubly_Linked_List.Node:" + text;
+        //
+        if (v instanceof Data_Value) text = "Data_Value:" + text;
+        if (v instanceof Data_Object) text = "Data_Object:" + text;
+        if (v instanceof Enhanced_Data_Object) text = "Enhanced_Data_Object:" + text;
+        if (v instanceof Collection) text = "Collection:" + text;
         //
         return text;
     }
 
-    var module = {
+
+    module.exports = {
         'toText': toText
-    }
+    };
 
-    return module;
 
-});
+    //var module = {
+    //    'toText': toText
+    //}
+
+    //return module;
+
+//});
