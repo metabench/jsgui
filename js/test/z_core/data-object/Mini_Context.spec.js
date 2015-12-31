101b6f4c5804ded28d594a68bdc00dab902e61c3
Sergey <nonexistent@example.com>
2015-12-31 05:22:30 +0500

    Mini_Context tests changes

--- js/test/z_core/data-object/Mini_Context.spec.js
+++ js/test/z_core/data-object/Mini_Context.spec.js
@@ -33,16 +33,15 @@ describe("z_core/data-object /Mini_Context.spec.js ", function () {
     //                  the test:
     // =========================================
 
-    it("!!! new_id() should throw", function () {
+    it("new_id()", function () {
         //
         var mini_context = new Data_Object.Mini_Context();
         //
-        //mini_context.new_id();
-        //
-        assert.throws(function () { mini_context.new_id(); }, function (err) { return err === "stop Mini_Context typed id"; });
+        // !!! does not work:
+        assert.throws(function () { mini_context.new_id(); }, "stop Mini_Context typed id"); // !!!
     });
 
-    it("make() should not works", function () {
+    it("make()", function () {
         //
         // ------------------------------
         jsgui.__data_id_method = "init";
@@ -50,24 +49,24 @@ describe("z_core/data-object /Mini_Context.spec.js ", function () {
         //
         var mini_context = new Data_Object.Mini_Context();
         //
-        assert.throws(function () { mini_context.make({}); }, function (err) { return err === "Object must be abstract, having ._abstract == true"; });
+        assert.throws(function () { mini_context.make({}); }, "Object must be abstract, having ._abstract == true");
         //
         var data_object = new Data_Object({ abstract: true });
         //
-        // !!!! exception!
-        assert.throws(function () { mini_context.make(data_object); }, function (err) { return err === "stop Mini_Context typed id"; });
+        // !!! does not work:
+        assert.throws(function () { mini_context.make(data_object); }, "stop Mini_Context typed id"); // !!!
         //
         //
         // ------------------------------
         jsgui.__data_id_method = "lazy";
         // ------------------------------
         //
-        assert.throws(function () { mini_context.make({}); }, function (err) { return err === "Object must be abstract, having ._abstract == true"; });
+        assert.throws(function () { mini_context.make({}); }, "Object must be abstract, having ._abstract == true");
         //
         var data_object2 = new Data_Object({ abstract: true });
         //
-        assert.throws(function () { mini_context.make(data_object2); }, "Reference error: r is not defined");
-
+        // !!! does not work:
+        assert.throws(function () { mini_context.make(data_object2); }, "Reference error: r is not defined"); // !!!
     });
 
 
